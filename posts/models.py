62cf4bbdff8fa9e81a0e9e38271cbb8446631f9d
kumargaurav5 <gauravdav2000@gmail.com>
2023-09-09 22:48:18 +0530

    first comment

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    body=models.CharField(max_length=10000)
+    created_at=models.DateField(default=datetime.now,blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
